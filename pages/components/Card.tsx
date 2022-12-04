f49b6e25f465bb5a97954d1491976fa8e2daf330
work2544 <work21_123@hotmail.com>
2022-12-04 21:34:35 +0700

    add prop type

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
-
-export default function Card(props: any) {
+export declare type props = {
+  fname: string;
+  job: string;
+  src: string;
+  github: string;
+  facebook: string;
+};
+export default function Card({ fname, job, src, github, facebook }: props) {
   return (
     <div className="flex justify-center align-middle w-full  ">
       <div className=" flex flex-col justify-center bg-white  w-64 p-4 rounded-lg backdrop-blur-md">
         <div className=" border-4 rounded-full ring-4 ring-blue-600">
-          <img className=" object-contain rounded-full w-64" src={props.src} />
+          <img className=" object-contain rounded-full w-64" src={src} />
         </div>
         <div className="flex flex-col justify-center text-center my-4">
-          <h1 className=" text-xl font-semibold">{props.fname}</h1>
-          <h1 className=" font-semibold text-violet-600">{props.job}</h1>
+          <h1 className=" text-xl font-semibold">{fname}</h1>
+          <h1 className=" font-semibold text-violet-600">{job}</h1>
         </div>
         <hr></hr>
         <div className="flex justify-center gap-3 mt-4">
-          <a href="https://github.com/thunyoubun">
+          <a href={github}>
             <AiFillGithub className=" hover:scale-110" size={32} />
           </a>
-          <a href="https://www.facebook.com/switch.2412">
+          <a href={facebook}>
             <FaFacebook size={32} className=" hover:scale-110" />
           </a>
         </div>
